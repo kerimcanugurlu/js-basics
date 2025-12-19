e62735599c1d83bc240d4a3fc2d86589e38c1b6e
kerimcanugurlu <149831686+kerimcanugurlu@users.noreply.github.com>
2025-12-20 02:17:38 +0300

    Add operators example
    Added examples of arithmetic and comparison operators in JavaScript.

---
+++ 03-operators.js
@@ -0,0 +1,21 @@
+// Operators in JavaScript
+
+// Aritmetic operators
+
+let a = 10;
+let b = 3;
+
+console.log(a + b); // addition
+console.log(a - b); // subtraction
+console.log(a * b); // multiplication
+console.log(a / b); // division
+console.log(a % b); // remainder
+
+// Comparison operators
+console.log(a > b);
+console.log(a < b);
+console.log(a === b);
+console.log(a !== b);
+
+
+
