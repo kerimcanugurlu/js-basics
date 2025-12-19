b42ea97dd78dff0c5fb7f0c825de8ecdcefa1fd0
kerimcanugurlu <149831686+kerimcanugurlu@users.noreply.github.com>
2025-12-19 05:00:01 +0300

    Add variables practice file

---
+++ 01-variables.js
@@ -0,0 +1,15 @@
+// 01-variables.js
+
+let name = "Kerimcan";
+const age = 21;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+let x = 10;
+let y = 5;
+
+console.log("x + y =", x + y);
+console.log("x - y =", x - y);
+console.log("x * y =", x * y);
+console.log("x / y =", x / y);
